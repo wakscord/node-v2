f4776fb37d6b9deea53cad26c32bb067a319c2db
cdw8431 <cdw8431@gmail.com>
2023-07-07 00:02:37 +0900

    fix(redis): C기반 redis 라이브러리인 hiredis 적용

--- app/infra/redis.py
+++ app/infra/redis.py
@@ -2,5 +2,5 @@ from redis.asyncio import ConnectionPool, Redis
 
 from app.common.settings import settings
 
-pool = ConnectionPool(host=settings.REDIS_URL, port=6379, password=settings.REDIS_PASSWORD)
-session: Redis = Redis(connection_pool=pool, decode_responses=True)
+pool = ConnectionPool(host=settings.REDIS_URL, port=6379, password=settings.REDIS_PASSWORD, decode_responses=True)
+session: Redis = Redis(connection_pool=pool)
