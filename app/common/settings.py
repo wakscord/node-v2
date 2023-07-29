f5007cce98c38baab264ac519cd07d8492af4431
cdw8431 <cdw8431@gmail.com>
2023-07-29 14:05:15 +0900

    feat(settings): 인스턴스의 명확한 구분을 위해서 uuid 대신 hostname으로 node_id 생성

--- app/common/settings.py
+++ app/common/settings.py
@@ -1,7 +1,7 @@
 import os
+import socket
 from dataclasses import dataclass
 from typing import Callable
-from uuid import uuid4
 
 from dotenv import dotenv_values, find_dotenv
 
@@ -14,11 +14,9 @@ if not os.path.exists(env_path):
 class Settings:
     NODE_ID: str
     MAX_CONCURRENT: int
-
     REDIS_URL: str
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str | None = None
-
     PROXY_USER: str | None = None
     PROXY_PASSWORD: str | None = None
 
@@ -27,7 +25,7 @@ to_int: Callable[[str, int], int] = lambda value, else_value: int(value) if valu
 
 raw_settings = dotenv_values(env_path)
 settings = Settings(
-    NODE_ID=f"node_{uuid4()}",
+    NODE_ID=f"node_{socket.gethostname()}",
     MAX_CONCURRENT=to_int(raw_settings.get("MAX_CONCURRENT"), 2000),
     REDIS_URL=raw_settings.get("REDIS_URL") or "localhost",
     REDIS_PORT=to_int(raw_settings.get("REDIS_PORT"), 6379),
