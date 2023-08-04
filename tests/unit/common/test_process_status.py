79688e348f3ed8c3aefb3b611f05b27acd934f39
cdw8431 <cdw8431@gmail.com>
2023-08-04 20:22:19 +0900

    test(common): process_status async 필요 없는 부분 제외

--- tests/unit/common/test_process_status.py
+++ tests/unit/common/test_process_status.py
@@ -6,15 +6,13 @@ from pytest_mock import MockerFixture
 from app.common.process_status import ProcessStatus, ProcessStatusManager, process_status_handler
 
 
-@pytest.mark.asyncio
-async def test_process_status_start():
+def test_process_status_start():
     manager = ProcessStatusManager()
     manager.start()
     assert manager.get_current() == ProcessStatus.RUNNING
 
 
-@pytest.mark.asyncio
-async def test_process_status_complete():
+def test_process_status_complete():
     manager = ProcessStatusManager()
     manager.complete()
     assert manager.get_current() == ProcessStatus.WAITING
